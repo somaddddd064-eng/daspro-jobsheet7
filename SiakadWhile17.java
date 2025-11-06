6e5b99b89e6c8577e9ea098d47d54afdff01623b
somaddddd064-eng <somaddddd064@gmail.com>
2025-11-06 14:48:42 +0700

    jobsheet 7 (2.2.1)

---
+++ SiakadWhile17.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class SiakadWhile17 {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int grade, total, i = 0;
+
+        System.out.println("Enter the number of student : ");
+        total = sc.nextInt();
+
+        while (i < total ) {
+           System.out.print("Enter the grade of the" + (i + 1) +"student :");
+           grade = sc.nextInt();
+
+           if (grade <= 0 || grade >= 100) {
+            System.out.println("Invalid grade. Re-enter a valid grade");
+            continue;
+           }
+
+           if (grade > 80 && grade <=100) {
+            System.out.println("The grade of the" +(i + 1)+ "student is A");
+           } else if (grade > 73 && grade <=80) {
+            System.out.println("The grade of the" + (i + 1)+ "student is B+");
+           } else if (grade > 65 && grade <= 73) {
+            System.out.println("The grade of the" + (i + 1)+ "student is B");
+           } else if (grade > 60 && grade <= 65) {
+            System.out.println("The grade of the" + (i + 1)+ "student is C+");
+           } else if (grade > 50 && grade <= 60) {
+            System.out.println("The grade of the" + (i + 1)+ "student is C");
+           } else if (grade > 39 && grade <= 50) {
+            System.out.println("The grade of the" + (i + 1) + "student is D");
+           } else {// grade <= 39
+            System.out.println("The grade of the " + (i + 1) + "student is E");}
+            i++;
+
+            }
+        }
+
+    }
+
