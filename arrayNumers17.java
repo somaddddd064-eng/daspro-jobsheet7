bc75209e14af2f966c0d39af3fe38c4dade19ef7
somaddddd064-eng <somaddddd064@gmail.com>
2025-11-20 12:28:00 +0700

    Array 1, 2.1

---
+++ arrayNumers17.java
@@ -0,0 +1,17 @@
+public class arrayNumers17 {
+
+    public static void main(String[] args){
+
+        double[] num = new double [4];
+        num [0] = 5.0; 
+        num [1] = 12867;
+        num [2] = 7.5;
+        num [3] = 2000000;
+
+        System.out.println(num [0]);
+        System.out.println(num [1]);
+        System.out.println(num [2]);
+        System.out.println(num [3]);
+
+    }
+}
