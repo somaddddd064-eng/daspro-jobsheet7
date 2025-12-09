4c8aef65cf03f4b12326f74de703b65d899e308b
somaddddd064-eng <somaddddd064@gmail.com>
2025-12-09 20:57:38 +0700

    question number 1

--- arrayAverageScore17.java
+++ arrayAverageScore17.java
@@ -10,7 +10,7 @@ public class arrayAverageScore17 {
 
         int[] score = new int [manyStudent];
         double total = 0;
-        double average;
+        double average1;
 
         for (int i = 0; i < score.length; i++){
             System.out.print("Enter student score " + ( i + 1) + " :");
@@ -24,11 +24,15 @@ public class arrayAverageScore17 {
             if (score[i] > 70){
                 System.out.println("Student " + (i + 1) + " :" + score[i]);
                 i++;
+            } else if (score[i] < 70){
+                System.out.println("Student " +(i + 1)+ "not passed" + score[i]);
+                i++;
             }
-        }
-        average = total / score.length;
+
+        average1 = total / score.length;
 
 
-        System.out.println("The class average score is " + average); 
+        System.out.println("The class average score is " + average1); 
     }
+}
 }
